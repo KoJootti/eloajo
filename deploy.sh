553f4ae3a418109ee1a599ce0f03debdbcab9959
Joonas Koistinen <jtkoistinen@gmail.com>
2023-06-18 22:46:05 +0300

    minor updates

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,6 @@ then
     exit 1;
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
 echo "Removing existing files"
 rm -rf public/*
 
@@ -22,7 +13,4 @@ echo "Generating site"
 git submodule update --init --recursive --depth=1
 hugo --minify
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-git push --all
